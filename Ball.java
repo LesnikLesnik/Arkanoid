58d97ba4770600e4f7bf40a718b58fb8ec86d772
LesnikLesnik <133019465+LesnikLesnik@users.noreply.github.com>
2023-07-01 15:37:32 +0300

    Add files via upload

--- Ball.java
+++ Ball.java
@@ -4,6 +4,35 @@ public class Ball extends BaseObject {
     public Ball(double x, double y, double radius) {
         super(x, y, radius);
     }
+    private double speed;
+    private double direction;
+    private double dx;
+    private double dy;
+    private boolean isFrozen;
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public double getDirection() {
+        return direction;
+    }
+
+    public double getDx() {
+        return dx;
+    }
+
+    public double getDy() {
+        return dy;
+    }
+
+    public Ball(double x, double y, double speed, double direction) {
+        super(x, y, 1 );
+        this.direction = direction;
+        this.speed = speed;
+        isFrozen = true;
+
+    }
 
     @Override
     public void draw(Canvas canvas) {
